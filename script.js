3543d7c4bbc58a4ce72c62bb088447364bd57097
Raunak Kumar Sah <itisraunak@gmail.com>
2023-08-19 16:13:16 +0530

    changes i have made
    Signed-off-by: Raunak Kumar Sah <itisraunak@gmail.com>

--- script.js
+++ script.js
@@ -1,22 +1,20 @@
-const key = '91470c81';
+const key = "91470c81";
 
+var searchInput = document.getElementById("Input");
+var displaySearchList = document.getElementsByClassName("fav-container");
 
-var searchInput = document.getElementById('Input');
-var displaySearchList = document.getElementsByClassName('fav-container');
-
-fetch('http://www.omdbapi.com/?i=tt3896198&apikey=91470c81')
-.then(res => res.json())
-.then(data => console.log(data))
-.catch(err => console.log(err));
+fetch("http://www.omdbapi.com/?i=tt3896198&apikey=91470c81")
+    .then((res) => res.json())
+    .then((data) => console.log(data))
+    .catch((err) => console.log(err));
 
 // upon keypress - function findMovies is initiated
-searchInput.addEventListener('input' , findMovies);
-
+searchInput.addEventListener("input", findMovies);
 
 async function singleMovie() {
     //finding the ID of the movie from the url
     var urlQueryParams = new URLSearchParams(window.location.search);
-    var id = urlQueryParams.get('id');
+    var id = urlQueryParams.get("id");
     console.log(id);
 
     const url = `https://www.omdbapi.com/?i=${id}&apiKey=${key}`;
@@ -60,49 +58,48 @@ async function singleMovie() {
             &thinsp; ${data.Awards}
         </p>
     </div> 
-    `
+    `;
 
     //Appending the output
 
-    document.querySelector('.movie-container').innerHTML = output;
-
+    document.querySelector(".movie-container").innerHTML = output;
 }
 
 // add to favorite
 
 async function addTofavorites(id) {
-    console.log("fav-item",id);
+    console.log("fav-item", id);
 
-    localStorage.setItem(Math.random().toString(36).slice(2,7) , id); // math.random for the unique key and value pair
-    alert('Movie Added to watchlist!');
+    localStorage.setItem(Math.random().toString(36).slice(2, 7), id); // math.random for the unique key and value pair
+    alert("Movie Added to watchlist!");
 }
 
 // removing the movie from the favorites list and also from the localstorage
 async function removeFromFavorites(id) {
     console.log(id);
-    for(i in localStorage) {
+    for (i in localStorage) {
         // if the id passed as argument matches with the value associated with the key , then removing it
-        if(localStorage[i] == id) {
-            localStorage.removeItem(i)
+        if (localStorage[i] == id) {
+            localStorage.removeItem(i);
             break;
         }
     }
 
     // alerting the user and refreshing the page
-    alert('Movie Removed From WatchList');
-    window.location.replace('favorite.html');
+    alert("Movie Removed From WatchList");
+    window.location.replace("favorite.html");
 }
 
 // displaying the movie list on the search page according to the user list
 async function displayMovieList(movies) {
-    var output = '';
-    // transversing over the movies list which is passed as an argument to our function 
-    for(i of movies) {
-        var img = '';
-        if(i.Poster != 'N/A') {
+    var output = "";
+    // transversing over the movies list which is passed as an argument to our function
+    for (i of movies) {
+        var img = "";
+        if (i.Poster != "N/A") {
             img = i.Poster;
         } else {
-            img = 'img/blank-poster.webp';
+            img = "img/blank-poster.webp";
         }
         var id = i.imdbID;
 
@@ -126,20 +123,20 @@ async function displayMovieList(movies) {
             </div>
         </div>
 
-       `
+       `;
     }
     // Appending this to the movie-display class of out html page
-    document.querySelector('.fav-container').innerHTML = output;
-    console.log("Here is movieList....." , movies);
+    document.querySelector(".fav-container").innerHTML = output;
+    console.log("Here is movieList.....", movies);
 }
 
 // when the user is searching for the movie then a list of the related movie will be displayed and that list is defined
 async function findMovies() {
-    const url = `https://www.omdbapi.com/?s=${(searchInput.value).trim()}&page=1&apikey=${key}`;
+    const url = `https://www.omdbapi.com/?s=${searchInput.value.trim()}&page=1&apikey=${key}`;
     const res = await fetch(`${url}`);
     const data = await res.json();
 
-    if(data.Search) {
+    if (data.Search) {
         //calling the function to display list to the movies related to the user search
         displayMovieList(data.Search);
     }
@@ -147,29 +144,32 @@ async function findMovies() {
 
 //Favorite movies are loaded on to the fav page from localstorage
 async function favoritesMovieLoader() {
-
-    var output = '';
+    var output = "";
     //Traversing over all the movies in the localstorage
-    for(i in localStorage) {
+    for (i in localStorage) {
         var id = localStorage.getItem(i);
-        if(id != null) {
+        if (id != null) {
             //Fetching the movie through id
-            const url = `http://www.omdbapi.com/?i=${id}&plot=full&apikey=${key}`;
+            const url = fetch(
+                `http://www.omdbapi.com/?i=${id}&plot=full&apikey=${key}`,
+                {
+                    referrerPolicy: "unsafe_url",
+                }
+            );
             // const url = `https://www.omdbapi.com/?i=${i}&plot=full&apikey=${key}`;  // id is on the localStorage
-            console.log(id ,"Start");
+            console.log(id, "Start");
             const res = await fetch(`${url}`);
             const data = await res.json();
             console.log(data);
 
-
-            var img = '';
-            if(data.Poster) {
-                img = data.Poster
+            var img = "";
+            if (data.Poster) {
+                img = data.Poster;
             } else {
                 img = data.Title;
             }
             var Id = data.imdbID;
-            // adding all the movie html in the output using interpolation 
+            // adding all the movie html in the output using interpolation
             output += `
 
             <div class="fav-item">
@@ -194,5 +194,5 @@ async function favoritesMovieLoader() {
         }
     }
     //Appending the html to the movie-display class in favourite page
-    document.querySelector('.fav-container').innerHTML = output;
+    document.querySelector(".fav-container").innerHTML = output;
 }
